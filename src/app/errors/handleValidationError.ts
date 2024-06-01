8523ee086e9cf3b26d3367e7677522c5851cc9a9
noyon-podder <noyon.podder7@gmail.com>
2024-06-01 16:47:03 +0600

    handle validation error

--- src/app/errors/handleValidationError.ts
+++ src/app/errors/handleValidationError.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose'
-import { TErrorSources } from '../interface/error.interface'
+import { TErrorSources, TGenericResponse } from '../interface/error.interface'
 
-const handleValidationError = (err: mongoose.Error.ValidationError) => {
+const handleValidationError = (
+  err: mongoose.Error.ValidationError,
+): TGenericResponse => {
   const errorSources: TErrorSources = Object.values(err.errors).map(
     (val: mongoose.Error.ValidatorError | mongoose.Error.CastError) => {
       return {
